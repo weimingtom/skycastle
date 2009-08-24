6f4de813168b0b0f7dddf943b97492dcebaf53ee
Hans.Haggstrom <Hans.Haggstrom@e1ebb57e-901a-11de-9a8c-6d5b7d1e4d13>
2009-08-24 01:38:09 +0000

    Added graphical dialog to client for testing webstart.

--- src/main/scala/org/skycastle/client/SkycastleClient.scala
+++ src/main/scala/org/skycastle/client/SkycastleClient.scala
@@ -1,8 +1,9 @@
 package org.skycastle.client
 
 
+import java.awt.Dimension
 import java.util.logging.Logger
-
+import javax.swing.{JLabel, JFrame}
 /**
  * Client side entrypoint 
  * 
@@ -15,7 +16,14 @@ object SkycastleClient {
   def main( argv: Array[String]) {
 
     logger.info( "Skycastle Client Started" )
-    
+
+    val frame = new JFrame()
+    frame.setTitle("Skycastle Client")
+    frame.setContentPane( new JLabel( "Skycastle Client Test" ) )
+    frame.setPreferredSize( new Dimension(800, 600) )
+    frame.pack
+    frame.setVisible(true)
+
   }
 
 
