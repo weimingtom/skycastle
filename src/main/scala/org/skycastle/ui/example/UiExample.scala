af6f638e4f56da6665c97902d32b62505b007f2d
Hans.Haggstrom <Hans.Haggstrom@e1ebb57e-901a-11de-9a8c-6d5b7d1e4d13>
2009-08-26 23:04:40 +0000

    some updates on the ui example

--- src/main/scala/org/skycastle/ui/example/UiExample.scala
+++ src/main/scala/org/skycastle/ui/example/UiExample.scala
@@ -18,8 +18,11 @@ object UiExample {
     screen.addUiComponent( 'panel, 'root, null, Parameters( Map( 'layout -> "" ) ) )
     screen.addUiComponent( 'label, 'testlabel_1, 'root, Parameters( Map( 'text -> "Test Label 1", 'tooltip -> "Example label 1" ) ) )
     screen.addUiComponent( 'label, 'testlabel_2, 'root, Parameters( Map( 'text -> "Test Label 2", 'tooltip -> "Example label 2" ) ) )
-    screen.addUiComponent( 'button, 'testButton_1, 'root, Parameters( Map( 'text -> "Click Me", 'tooltip -> "Test Button", 'invokedEntity -> EntityId("entity-432455"), 'invokedMethod -> "testAction" ) ) )
+    screen.addUiComponent( 'panel, 'panel2, 'root, Parameters( Map( 'layout -> "" ) ) )
+    screen.addUiComponent( 'button, 'testButton_1, 'panel2, Parameters( Map( 'text -> "Click Me", 'tooltip -> "Test Button", 'invokedEntity -> EntityId("entity-432455"), 'invokedMethod -> "testAction" ) ) )
 
+    screen.removeUiComponent( 'testlabel_1 )
+    
     val view = screen.getView
 
     new SimpleFrame( "UiExample", view)
