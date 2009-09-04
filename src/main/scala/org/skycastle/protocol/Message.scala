986afd23d2cdb2311c7a581a888b01a8b3d06dd4
Hans.Haggstrom <Hans.Haggstrom@e1ebb57e-901a-11de-9a8c-6d5b7d1e4d13>
2009-09-04 16:30:07 +0000

    Sketched out the protocol interfaces.

---
+++ src/main/scala/org/skycastle/protocol/Message.scala
@@ -0,0 +1,16 @@
+package org.skycastle.protocol
+
+
+import entity.EntityId
+import util.Parameters
+
+/**
+ * The messages passed between the server and clients.
+ *
+ * Consists of an action call on a specified entity.
+ * 
+ * @author Hans Haggstrom
+ */
+@serializable
+@SerialVersionUID(1)
+case class Message( calledEntity : EntityId, calledAction : String, parameters : Parameters )
---
+++ src/main/scala/org/skycastle/protocol/Protocol.scala
@@ -0,0 +1,33 @@
+package org.skycastle.protocol
+
+
+import java.nio.ByteBuffer
+
+/**
+ * A way of encoding and decoding Messages to and from a stream of bytes.
+ * Used for communication between client and server.
+ * 
+ * @author Hans Haggstrom
+ */
+abstract class Protocol  {
+
+  /**
+   * The name of the protocol, defined by implementing classes.
+   */
+  val protocolName : String
+
+  /**
+   * Encodes a message to a ByteBuffer
+   * Throws an Exception if there was some problem.
+   */
+  def encode( message : Message) : ByteBuffer
+
+  /**
+   * Decodes a message from a ByteBuffer to a Message
+   * Throws an Exception if there was some problem.
+   */
+  def decode( receivedBytes : ByteBuffer  ) : Message
+  
+}
+
+
