0f07ab64bbce8f961b5e70b58d5dc40a6cf56815
Hans.Haggstrom <Hans.Haggstrom@e1ebb57e-901a-11de-9a8c-6d5b7d1e4d13>
2009-09-08 15:59:48 +0000

    Added println

--- src/test/scala/org/skycastle/entity/expression/ExpressionTest.scala
+++ src/test/scala/org/skycastle/entity/expression/ExpressionTest.scala
@@ -20,7 +20,7 @@ class ExpressionTest extends TestCase {
 
     val expr : Expression = Num(5) * Num(10) + NumProp( 'someValue, Num(0) ) < Num( 10) and Not( Prop( 'alarm, Const("none") ) sameAs Const("high"))
 
-    expr.toString
+    println  ( expr.toString )
 
   }
 
