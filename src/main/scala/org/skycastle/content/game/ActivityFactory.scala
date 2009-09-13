a2d343b1b50764e7f6db6b3c8c6da40e21648da7
Hans.Haggstrom <Hans.Haggstrom@e1ebb57e-901a-11de-9a8c-6d5b7d1e4d13>
2009-09-13 12:43:31 +0000

    Activity factory sketching

---
+++ src/main/scala/org/skycastle/content/game/ActivityFactory.scala
@@ -0,0 +1,26 @@
+package org.skycastle.content.game
+
+import _root_.org.skycastle.entity.Entity
+import _root_.org.skycastle.util.{Properties, Parameters}
+/**
+ * 
+ */
+
+class ActivityFactory extends Entity {
+
+
+  /**
+   * Return calls with a description of the activities created by this factory.
+   */
+  def getDescription( caller : Entity )
+
+  /**
+   * Description of the activities created by this factory.
+   */
+  def description : Parameters
+
+  def createActivity( parameters : Parameters )
+
+  
+
+}
