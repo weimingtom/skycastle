faa6b30ed7bd3ea890715c45acf477460c360170
Hans.Haggstrom <Hans.Haggstrom@e1ebb57e-901a-11de-9a8c-6d5b7d1e4d13>
2009-09-25 10:07:53 +0000

    Documentation

--- src/main/scala/org/skycastle/network/Message.scala
+++ src/main/scala/org/skycastle/network/Message.scala
@@ -8,6 +8,12 @@ import util.Parameters
  * The messages passed between the server and clients.
  *
  * Consists of an action call on a specified entity.
+ *
+ * The parameters contains the data to be passed to the receiving action.
+ * The network system can handle transfer of at least the basic primitive types, (scala) lists, sets, and maps,
+ * as well as EntityID:s, Symbols, Strings, and Parameters.  The coder can also define own transferable types
+ * by implementing the Transferable interface, which should convert an object to and from some composition
+ * of the types listed above. 
  * 
  * @author Hans Haggstrom
  */
